a5d01d8858f9ccb09253c4258def7da433ad4198
kavyajeetbora <kavyajeetbora@gmail.com>
2025-10-09 22:28:09 +0530

    Ädd - vector store for categories

--- src/db/duckdb_utils.py
+++ src/db/duckdb_utils.py
@@ -2,6 +2,14 @@
 Database utility functions for DuckDB and other DBs.
 """
 import duckdb
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.documents import Document
+from langchain_chroma import Chroma
+import os
+from glob import glob
+import shutil
+import logging
+import randomname
 
 def get_duckdb_connection(database: str) -> duckdb.DuckDBPyConnection:
     """Create and configure a DuckDB connection."""
@@ -49,6 +57,96 @@ def create_places_with_categories_view_and_export(
     finally:
         con.close()
 
+def create_category_list(vector_db_dir: str):
+    """
+    Creates a distinct list of categories from the places parquet files.
+    Args:
+        s3_places_path (str): S3 path to places parquet files.
+    """
+
+    ## 1. First Get all the Distinct Categories from Places
+    # Initialize DuckDB connection
+    con = duckdb.connect()
+
+    # Load required extensions
+    con.execute("INSTALL httpfs; LOAD httpfs; INSTALL spatial; LOAD spatial;")
+
+    s3_places_path = 's3://fsq-os-places-us-east-1/release/dt=2025-09-09/places/parquet/places-*.zstd.parquet'
+
+    # Execute the SELECT query and create a view
+    result = con.execute(f"""
+    SELECT
+        DISTINCT UNNEST(fsq_category_labels) as category
+    FROM read_parquet('{s3_places_path}') WHERE country='IN';
+    """).df()
+
+    con.close()
+
+    ## 2. Intialize the Embedding Model
+    logging.info("Downloading Embeddings Model   ")
+    embeddings = HuggingFaceEmbeddings(model_name="Qwen/Qwen3-Embedding-0.6B")
+    logging.info("Embeddings Model Downloaded   ")
+
+    ## 3. Create the Vector DB
+    logging.info("Creating Embeddings for Categories")
+
+    ## First Create documents
+    categories = result['category'].drop_duplicates().tolist()
+
+    documents = [
+            Document(
+                page_content=cat, metadata={"category_id": i, "source": "foursquare poi"}
+            ) for i,cat in enumerate(categories)
+        ]
+
+    logging.info(f"Created {len(documents)} documents for categories")
+
+    ## 4. Create the Vector DB into Local Path
+    ## Now Store the Embeddings in Vector Store
+
+    
+
+    logging.info("Creating Vector DB Directory if not exists")
+    os.makedirs("db", exist_ok=True)
+
+    random_suffix = randomname.get_name()
+
+    persistent_directory = f"{vector_db_dir}/chroma-({random_suffix})"
+
+    ## If already there, delete and create a new one
+    for folder in glob(f"{vector_db_dir}/chroma*"):
+        if os.path.exists(folder):
+            shutil.rmtree(folder)
+
+    os.mkdir(persistent_directory)
+
+    logging.info(f"Created Vector DB Directory at {persistent_directory}")
+
+    logging.info("Creating Vector DB for Categories")
+    vector_db = Chroma.from_documents(
+        documents=documents,
+        collection_name="poi_category_embeddings",
+        embedding=embeddings,  # Now LangChain-compatible!
+        persist_directory=persistent_directory
+    )
+    logging.info("Created Vector DB to Disk")
+
+
+def load_vector_db(path, collection_name="poi_category_embeddings"):
+    if os.path.exists(path):
+        # Same embedding model as creation—critical for query consistency
+        embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+        
+        vector_db = Chroma(
+            persist_directory=path,
+            collection_name=collection_name,
+            embedding_function=embedding
+        )
+        logging.info(f"Loaded vector DB from {path} with collection {collection_name}")
+        return vector_db
+    else:
+        logging.error(f"Path {path} not found!")
+        return None
 
 
 if __name__ == "__main__":
